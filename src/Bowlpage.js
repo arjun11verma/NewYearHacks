db88809b2e9cf3c066841b415549c9c389d00142
Owen Petersen <oweneisser@gmail.com>
2021-01-01 18:25:53 -0500

    shutfffin

--- src/Bowlpage.js
+++ src/Bowlpage.js
@@ -1,21 +1,49 @@
 import React, {Component} from 'react';
-import {Grid, Paper, Typography} from '@material-ui/core/';
+import {Grid, Paper, Typography, Button, Container, TextField} from '@material-ui/core/';
 import axios from 'axios';
 import base_url from './APIENDPOINT';
-import { red } from '@material-ui/core/colors';
+
+
+
 
 class Bowlpage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             questionText: "",
-            maxQuestions: []
+            questionNums: this.range(1,99),
+            qcount: 0,
+            maxQuestions: 0,
+            activeQuestion: ""
         }
     }
 
+    range = (min, max) => {
+        var a = [];
+        for (var i = min; i <= max; i++) {
+            a.push(i)
+        }
+        for (var j = 0; j < a.length; j++) {
+            a = this.swap(a, j)
+        }
+        return a;
+    }
+
+    swap = (arr, i) => {
+        var temp = arr[i];
+        var ri = Math.floor(Math.random() *arr.length);
+        arr[i] = arr[ri];
+        arr[ri] = temp;
+        return arr;
+    }
+    
+
     addToQuestion = () => {
-        var questionFromDB, nextWord, count = 0, questionNum = this.state.maxQuestions[Math.floor(Math.random() * this.state.maxQuestions.length)];
-        axios.post(base_url + 'getQuestions', {'user': false, 'ID': questionNum}).then((res) => {
+        var questionFromDB, nextWord, count = 0; //Math.floor(Math.random() * this.state.maxQuestions.length);
+        axios.post(base_url + 'getQuestions', {'user': false, 'ID': this.state.questionNums[this.state.qcount]}).then((res) => {
+            this.setState({
+                activeQuestion: res.data.questionList[0]
+            });
             questionFromDB = res.data.questionList[0].questionbody.split(" ");
             console.log(questionFromDB);
             var questionInterval = setInterval(() => {
@@ -28,18 +56,30 @@ class Bowlpage extends Component {
                 count++;
                 if(count === questionFromDB.length) {
                     clearInterval(questionInterval);
-                    this.setState((prevState) => {
-                        return{
-                            maxQuestions: prevState.maxQuestions.splice(questionNum, 1)
-                        }
-                    });
+                    
                 }
             }, 100);
         });
     }
 
-    getRandomNumber = () => {
+    nextQuestion = () =>{
+        this.setState((prevState) => {
+            return{
+                qcount: prevState.qcount + 1,
+                questionText: ""
+            }
+        }, () => {
+            this.addToQuestion();
+        });
+    }
 
+    checkAnswer = () =>{
+        const answer = document.getElementById('answerText').value;
+        const answers = this.state.activeQuestion.questionanswer.split(" ");
+
+        console.log(answer, answers);
+        
+        return answers.includes(answer);
     }
 
     componentDidMount = () => {
@@ -61,7 +101,7 @@ class Bowlpage extends Component {
             papers: {
                 backgroundColor: "whitesmoke",
                 height: 350,
-                width: 'auto',
+                width: 950,
                 margin: 10,
                 display: 'flex',
                 flexWrap: 'wrap',
@@ -70,15 +110,30 @@ class Bowlpage extends Component {
 
         return (
             <div >
-                <Grid container className={styles.root} >
+                <Grid container style={styles.root} >
                     <Grid item xs={8}> 
+                    <Grid container direction = "row">
                         <Paper style={styles.papers} elevation={3}> 
                             <Typography>{this.state.questionText}</Typography>
                         </Paper>
+                        <Container align = "center">
+                            <Button variant="contained" onClick={this.nextQuestion}>Next Question</Button>
+                            <Button variant="contained" onClick={this.checkAnswer}>Submit Answer</Button>
+                        </Container>
+                        <Container align="center">
+                            <Grid item xs={4}>
+                                <TextField id="answerText" label="Answer">Answer</TextField>
+                            </Grid>
+                        </Container>
+                    </Grid>
                     </Grid>
                     <Grid item xs={4}>
                         <h1> hi right side</h1>
                     </Grid>
+                    <Grid item xs={1}>
+
+                    </Grid>
+
                 </Grid>
             </div>
         )
