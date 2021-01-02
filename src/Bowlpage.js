af351b0f4f2fad16a03a85134b3bc2854dec46c3
Nathanael <Nathanael.Oliver437@gmail.com>
2021-01-02 16:56:46 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/Bowlpage.js
+++ src/Bowlpage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Grid, Paper, Typography, Button, Container, TextField } from '@material-ui/core/';
-import axios from 'axios';
 import { base_url, socket_url } from './APIENDPOINT';
 import socketIOClient from 'socket.io-client';
 import Appbar from './Appbar';
@@ -75,11 +74,76 @@ class Bowlpage extends Component {
         }
     }
 
-    parseAnswer = (answer) => {
-        const d = "arjunvermaarjun";
-        const accept = answer.replace(";", d).replace("[", d).replace("]", d).replace("or", d).replace("OR", d).replace(",", d).split(d);
-        return accept
+    removeParentheses = (string) => {
+        
+        if (string.includes("(")) {
+            var startI = 0, pCount = 0, removeSub = []
+            for(var i = 0; i < string.length; i++) {
+                if (string.charAt(i) ===  "(") {
+                    if (pCount === 0) {
+                        startI = i
+                    }
+                    pCount ++;
+                } else if (string.charAt(i) === ")") {
+                    pCount --;
+                    if (pCount === 0) {
+                        removeSub.push([startI, i])
+                    }
+                }
+            }
+            for (var j = removeSub.length - 1; j >= 0; j--) {
+                string = string.substr(0, removeSub[j][0]) + " " + string.substr(removeSub[j][1] + 1, string.length)
+            }
+        }
+        return string
     }
+
+    parseAnswer = (answer) => {
+        console.log(answer)
+        
+        var setting = "A: "
+        var d = "QQQQQQQQ"
+        var myAns = this.removeParentheses(answer).replace(/[\s[]or\s/gi, d).replace(/\slike\s/gi).replace("[", d).replace(/;/g,d).replace("]", d).replace(/,/g, d).split(d).filter(function (el) {
+            return el !== null && el !== undefined && el !== ''
+        }).map((parsedAnswer) => {
+            parsedAnswer = parsedAnswer.trim()
+            if (parsedAnswer.includes(' ')) {
+                var cutOff = 0
+                if (parsedAnswer.search(/accept\s/gi) !== -1) {
+                    setting = "A: ";
+                    cutOff = parsedAnswer.search(/accept\s/gi) + 7
+                } else if (parsedAnswer.search(/prompt\son\s/gi) !== -1) {
+                    setting = "P: ";
+                    cutOff = parsedAnswer.search(/prompt\son\s/gi) + 10
+                } else if (parsedAnswer.search(/prompt\s/gi) !== -1) {
+                    setting = "P: ";
+                    cutOff = parsedAnswer.search(/prompt\s/gi) + 7
+                    
+                }
+                parsedAnswer = parsedAnswer.substr(cutOff, parsedAnswer.length);
+                cutOff = 0
+                var variationsI = parsedAnswer.search(/such\sas/gi)
+                if (variationsI !== -1) {
+                    cutOff = variationsI + 7
+                }
+                parsedAnswer = parsedAnswer.substr(cutOff, parsedAnswer.length);
+                cutOff = 0
+                variationsI = parsedAnswer.search(/that\sindicate/gi);
+                if (variationsI !== -1) {
+                    cutOff = variationsI + 13
+                }
+                parsedAnswer = parsedAnswer.substr(cutOff, parsedAnswer.length);
+            }
+            if (parsedAnswer === " " || parsedAnswer === "") {
+                return null
+            }
+            return setting + parsedAnswer;
+        }).filter(function (el) {
+            return el !== null
+        });
+        return myAns;
+    }
+
 
     checkAnswer = () => {
         const answer = document.getElementById('answerText').value;
